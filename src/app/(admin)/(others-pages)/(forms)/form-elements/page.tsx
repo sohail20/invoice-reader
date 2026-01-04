d4306dbdece8927abab102c12fd6457e3ca732ce
sohail <sbhatti@sunbonn.com>
2026-01-05 01:48:27 +0500

    code

--- src/app/(admin)/(others-pages)/(forms)/form-elements/page.tsx
+++ src/app/(admin)/(others-pages)/(forms)/form-elements/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import CheckboxComponents from "@/components/form/form-elements/CheckboxComponents";
 import DefaultInputs from "@/components/form/form-elements/DefaultInputs";
@@ -9,14 +11,11 @@ import RadioButtons from "@/components/form/form-elements/RadioButtons";
 import SelectInputs from "@/components/form/form-elements/SelectInputs";
 import TextAreaInput from "@/components/form/form-elements/TextAreaInput";
 import ToggleSwitch from "@/components/form/form-elements/ToggleSwitch";
-import { Metadata } from "next";
-import React from "react";
 
-export const metadata: Metadata = {
-  title:
-    "Invoice Reader Dashboard | AdminPanel",
-  description: "SAAS Base Invoice Reader Dashboard",
-};
+// export const metadata: Metadata = {
+//   title: "Invoice Reader Dashboard | AdminPanel",
+//   description: "SAAS Base Invoice Reader Dashboard",
+// };
 
 export default function FormElements() {
   return (
@@ -25,7 +24,7 @@ export default function FormElements() {
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
         <div className="space-y-6">
           <DefaultInputs />
-          <SelectInputs onChange={()=>{}} options={[]}/>
+          <SelectInputs onChange={() => {}} options={[]} />
           <TextAreaInput />
           <InputStates />
         </div>
