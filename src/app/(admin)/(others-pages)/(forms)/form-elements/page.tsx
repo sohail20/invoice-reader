6d0371d219956143e7e461c799dc842861bb2e8d
sohail <sbhatti@sunbonn.com>
2026-01-05 01:23:19 +0500

    code

--- src/app/(admin)/(others-pages)/(forms)/form-elements/page.tsx
+++ src/app/(admin)/(others-pages)/(forms)/form-elements/page.tsx
@@ -25,7 +25,7 @@ export default function FormElements() {
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
         <div className="space-y-6">
           <DefaultInputs />
-          <SelectInputs />
+          <SelectInputs onChange={()=>{}} options={[]}/>
           <TextAreaInput />
           <InputStates />
         </div>
